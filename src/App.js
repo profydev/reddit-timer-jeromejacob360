69a8aaa24ae0d2740f82006c8f643f2e048a9e12
jeromejacob360 <92665803+jeromejacob360@users.noreply.github.com>
2021-10-31 09:29:46 +0530

    add header placeholder

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Home from './components/Home';
 import Search from './components/Search';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/search" component={Search} />
