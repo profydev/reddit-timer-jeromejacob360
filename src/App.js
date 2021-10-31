197facf4e4c81898edbdef15ff60eb7090cde242
jeromejacob360 <92665803+jeromejacob360@users.noreply.github.com>
2021-10-31 15:20:24 +0530

    create-app-skeleton (#1)
    * create-app-skeleton
    
    * routing fixed
    
    * add meta info
    
    * finalize app skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import theme from './theme';
+import Home from './components/Home';
+import Search from './components/Search';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <BrowserRouter>
+      <ThemeProvider theme={theme} />
+      <Switch>
+        <Route exact path="/search" component={Search} />
+        <Route exact path="/" component={Home} />
+        <Route>404 Not found</Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
